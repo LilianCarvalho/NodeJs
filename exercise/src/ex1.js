11f838d6a71f67b45b6a2689ecc7163c05144267
LilianCarvalho <liliancarol93@gmail.com>
2021-07-05 13:07:06 -0300

    Exercícios NodeJS (Ainda falta alguns detalhes)

---
+++ exercise/src/ex1.js
@@ -0,0 +1,12 @@
+//Criar um programa nodejs que faça uma chamada http em um site qualquer.
+
+const axios = require('axios');
+
+axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  .then(response => {
+    console.log(response.data.url);
+    console.log(response.data.explanation);
+  })
+  .catch(error => {
+    console.log(error);
+  });
